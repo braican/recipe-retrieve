b578ef9dfe535ae57722b4784069f425021f60b3
Nick Braica <nick.braica@gmail.com>
2019-06-27 14:29:07 -0700

    Homepage view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="login">
-    <h1>Recipe Retrieve</h1>
-    <button class="btn" @click="login">
-      Sign in with Google
-    </button>
+  <div class="home">
+    <div class="cover stack">
+      <h1 class="title">
+        Recipe Retrieve
+      </h1>
+      <p class="intro">
+        Your online recipes, in one place.
+      </p>
+      <br>
+      <button class="btn" @click="login">
+        Sign in with Google
+      </button>
+    </div>
 
-    <CookbookIcon />
+    <div class="art">
+      <CookbookIcon />
+    </div>
   </div>
 </template>
 
@@ -31,7 +41,52 @@ export default {
 <style lang="scss" scoped>
 @import '../styles/abstracts/abstracts';
 
+.home {
+  padding: 20px;
+  display: flex;
+  height: 100vh;
+  align-items: center;
+  background-color: $c--teal;
+  overflow: hidden;
+}
 
+.title, .intro {
+  color: $c--white;
+}
+
+.intro {
+  font-size: 1.5rem;
+}
+
+.art {
+  display: flex;
+  align-items: center;
+  order: -1;
+  width: 50vw;
+  height: 100%;
+  padding: 40px;
+  position: relative;
+
+  &:after {
+    content: '';
+    display: block;
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background-image: radial-gradient(circle closest-side, rgba(255, 255, 255, .3), rgba(255, 255, 255, 0));
+  }
+
+  svg {
+    position: relative;
+    z-index: 2;
+    width: 100%;
+    max-width: 400px;
+    display: block;
+    margin: auto;
+  }
+}
 
 </style>
 
