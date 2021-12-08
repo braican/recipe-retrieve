929233158c4171c603258db84a133037aaa3ea30
Nick Braica <nick.braica@gmail.com>
2021-12-07 17:36:45 -0800

    Scaffold from scratch

--- src/main.js
+++ src/main.js
@@ -1,18 +1,7 @@
-import Vue from 'vue';
-import { firestorePlugin } from 'vuefire';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import { auth } from '@/firebase';
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
 
-Vue.use(firestorePlugin);
-
-Vue.config.productionTip = false;
-
-auth.onAuthStateChanged(() => {
-  new Vue({
-    router,
-    store,
-    render: h => h(App),
-  }).$mount('#app');
-});
+createApp(App).use(store).use(router).mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,93 +1,18 @@
 <template>
   <div class="home">
-    <div class="cover stack">
-      <h1 class="title">
-        Recipe Retrieve
-      </h1>
-      <p class="intro">
-        Your online recipes, in one place.
-      </p>
-      <br>
-      <button class="btn" @click="login">
-        Sign in with Google
-      </button>
-    </div>
-
-    <div class="art">
-      <CookbookIcon />
-    </div>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import { auth, authProvider } from '@/firebase';
-import CookbookIcon from '@/svg/cookbook';
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Login',
-  components: { CookbookIcon },
-  methods: {
-    login() {
-      auth.signInWithPopup(authProvider).then(() => {
-        this.$router.replace('/recipes');
-      }).catch(err => {
-        console.error(err);
-      });
-    },
+  name: "Home",
+  components: {
+    HelloWorld,
   },
 };
 </script>
-
-<style lang="scss" scoped>
-@import '../styles/abstracts/abstracts';
-
-.home {
-  padding: 20px;
-  display: flex;
-  height: 100vh;
-  align-items: center;
-  background-color: $c--teal;
-  overflow: hidden;
-}
-
-.title, .intro {
-  color: $c--white;
-}
-
-.intro {
-  font-size: 1.5rem;
-}
-
-.art {
-  display: flex;
-  align-items: center;
-  order: -1;
-  width: 50vw;
-  height: 100%;
-  padding: 40px;
-  position: relative;
-
-  &:after {
-    content: '';
-    display: block;
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background-image: radial-gradient(circle closest-side, rgba(255, 255, 255, .3), rgba(255, 255, 255, 0));
-  }
-
-  svg {
-    position: relative;
-    z-index: 2;
-    width: 100%;
-    max-width: 400px;
-    display: block;
-    margin: auto;
-  }
-}
-
-</style>
-
-
