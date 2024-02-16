521c035f83dc2661c61c12ec95e098c2caedb278
Nick Braica <nick.braica@gmail.com>
2024-02-16 10:12:56 -0800

    Clear out

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  preset: "@vue/cli-plugin-unit-jest",
-  testMatch: [
-    "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)",
-    "**/src/**/*.spec.(js|jsx|ts|tsx)"
-  ]
-};
