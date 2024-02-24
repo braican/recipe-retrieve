29928d6cd596fc418188b085f2bcace40d6f49f0
Nick Braica <nick.braica@gmail.com>
2024-02-23 23:16:18 -0800

    Fix a few things

--- src/routes/(protected)/account/+page.server.ts
+++ src/routes/(protected)/account/+page.server.ts
@@ -1,42 +1,32 @@
-import { fail, redirect } from '@sveltejs/kit';
+// import { fail, redirect } from '@sveltejs/kit';
 
 export const actions = {
   update: async ({ request, locals: { supabase, session } }) => {
     const formData = await request.formData();
     const fullName = formData.get('fullName') as string;
-    const username = formData.get('username') as string;
-    const website = formData.get('website') as string;
-    const avatarUrl = formData.get('avatarUrl') as string;
 
-    const { error } = await supabase.from('profiles').upsert({
-      id: session?.user.id,
-      full_name: fullName,
-      username,
-      website,
-      avatar_url: avatarUrl,
-      updated_at: new Date(),
-    });
+    console.log(fullName);
 
-    if (error) {
-      return fail(500, {
-        fullName,
-        username,
-        website,
-        avatarUrl,
-      });
-    }
+    // const { error } = await supabase.from('profiles').upsert({
+    //   id: session?.user.id,
+    //   full_name: fullName,
+    //   username,
+    //   website,
+    //   avatar_url: avatarUrl,
+    //   updated_at: new Date(),
+    // });
+
+    // if (error) {
+    //   return fail(500, {
+    //     fullName,
+    //     username,
+    //     website,
+    //     avatarUrl,
+    //   });
+    // }
 
     return {
-      fullName,
-      username,
-      website,
-      avatarUrl,
+      test: 'one',
     };
-  },
-  signout: async ({ locals: { supabase, session } }) => {
-    if (session) {
-      await supabase.auth.signOut();
-      redirect(303, '/');
-    }
   },
 };
--- src/routes/(protected)/account/+page.svelte
+++ src/routes/(protected)/account/+page.svelte
@@ -5,8 +5,6 @@
   import { authStore } from '$lib/stores';
 
   const { user } = authStore;
-
-  export let form;
   let loading = false;
 
   const handleSubmit: SubmitFunction = () => {
@@ -17,24 +15,26 @@
   };
 </script>
 
-<div>
-  <form method="post" action="?/update" use:enhance={handleSubmit}>
-    <div>
-      <label for="email">Email</label>
-      <input id="email" type="text" value={$user?.email} disabled />
-    </div>
+{#if $user}
+  <div>
+    <form method="post" action="?/update" use:enhance={handleSubmit}>
+      <div>
+        <label for="email">Email</label>
+        <input id="email" type="text" value={$user.email} disabled />
+      </div>
 
-    <div>
-      <label for="fullName">Full Name</label>
-      <input id="fullName" name="fullName" type="text" value={$user?.email} />
-    </div>
+      <div>
+        <label for="fullName">Full Name</label>
+        <input id="fullName" name="fullName" type="text" value={$user.fullName} />
+      </div>
 
-    <div>
-      <input
-        type="submit"
-        class="button block primary"
-        value={loading ? 'Loading...' : 'Update'}
-        disabled={loading} />
-    </div>
-  </form>
-</div>
+      <div>
+        <input
+          type="submit"
+          class="button block primary"
+          value={loading ? 'Loading...' : 'Update'}
+          disabled={loading} />
+      </div>
+    </form>
+  </div>
+{/if}
